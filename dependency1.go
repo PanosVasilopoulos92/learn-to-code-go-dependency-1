fabe8ea53684a2021d8ac8c659150d072c3027f1
PanosV <panosVasilopoulos92@gmail.com>
2023-12-16 13:14:39 +0200

    Adds two functions.

---
+++ dependency1.go
@@ -0,0 +1,9 @@
+package dependency1
+
+func SayWelcome() string {
+	return "Welcome!"
+}
+
+func SayGoodBye() string {
+	return "Goodbye!"
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/PanosVasilopoulos92/learn-to-code-go-dependency-1
+
+go 1.21.5
