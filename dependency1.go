1f342754a786ed59e1e53413d3e4800829f22c1a
PanosV <panosVasilopoulos92@gmail.com>
2023-12-16 15:00:12 +0200

    Adds the function 'Leaving'

--- dependency1.go
+++ dependency1.go
@@ -1,9 +1,17 @@
 package dependency1
 
+import (
+	dependancy2 "github.com/PanosVasilopoulos92/learn-to-code-code-dependency-2"
+)
+
 func SayWelcome() string {
 	return "Welcome!"
 }
 
 func SayGoodBye() string {
 	return "Goodbye!"
+}
+
+func Leaving() string {
+	return dependancy2.WhenYouLeave(SayGoodBye())
 }
